b38ae99cf00b6b1e5df1f52d27500159e5fc588e
ashafaatadhis <skullreaper387@gmail.com>
2022-08-13 09:35:18 +0700

    add getUser

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const route = require("./routes/routes");
 const jwt = require("jsonwebtoken");
 const verifyToken = require("./middleware/verifyToken");
+const User = require("./config/model/User");
 
 app.use(cors());
 app.use(express.json({ type: "application/json" }));
@@ -20,8 +21,18 @@ app.use(route);
   }
 })();
 
-app.get("/getUser", verifyToken, (req, res) => {
-  console.log(req.body);
+app.get("/getUser", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    res.status(200).json({ msg: user });
+  } catch (err) {
+    res.status(400).json({ msg: "Unknown error" });
+  }
 });
 
 app.listen(5050, () => console.log("server listened..."));
